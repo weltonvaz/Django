26d66858daaebfed3e10d9cc0135e31c4cbc96a0
Welton Vaz <weltonvaz@gmail.com>
2018-08-27 14:50:18 -0300

    alterações da aula 08 em 27/08/2018

--- connectedin/connectedin/perfis/models.py
+++ connectedin/connectedin/perfis/models.py
@@ -1,10 +1,16 @@
-# connectedin/perfis/models.py
-
 from django.db import models
 
 class Perfil(models.Model):
 
-    nome = models.CharField(max_length=255, null=False)
-    email = models.CharField(max_length=255, null=False)
-    telefone = models.CharField(max_length=15, null=False)
-    nome_empresa = models.CharField(max_length=255, null=False)
+	nome = models.CharField(max_length=255, null=False)
+	email = models.CharField(max_length=255, null=False)     
+	telefone = models.CharField(max_length=15, null=False)
+	nome_empresa = models.CharField(max_length=255, null=False)
+
+	def convidar(self, perfil_convidado):
+		Convite(solicitante=self, convidado=perfil_convidado).save()
+
+class Convite(models.Model):
+
+	solicitante = models.ForeignKey(Perfil, related_name='convites_feitos')
+	convidado = models.ForeignKey(Perfil, related_name='convites_recebidos')
